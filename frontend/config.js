9770fdce50adebbdac7311297661fbb7926eada8
Shivam Atre <shivamatre003@gmail.com>
2026-08-24 12:25:14 +0530

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,8 +1,8 @@
 // Adjust these if you're not running via docker-compose on localhost,
 // e.g. point them at your EKS Ingress hostname later.
 const CONFIG = {
-  USER_SERVICE_URL: "http://108.130.20.168:5001",
-  ORDER_SERVICE_URL: "http://108.130.20.168:5000",
-  PAYMENT_SERVICE_URL: "http://108.130.20.168:5002",
-  NOTIFICATION_SERVICE_URL: "http://108.130.20.168:5003",
+    USER_SERVICE_URL: "http://108.130.20.168:5001",
+    ORDER_SERVICE_URL: "http://108.130.20.168:5000",
+    PAYMENT_SERVICE_URL: "http://108.130.20.168:5002",
+    NOTIFICATION_SERVICE_URL: "http://108.130.20.168:5003"
 };
